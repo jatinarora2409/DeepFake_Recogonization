4b4f65cd39754a2278b27b65759a0a9d92a3db5b
jatinarora <jatinarora2409@gmail.com>
2020-02-23 21:19:16 -0600

    The base first commit to recoginze images

---
+++ CV_DeepFake/base.py
@@ -0,0 +1,60 @@
+from IPython.core.display import display
+from sklearn.metrics import log_loss
+import cv2 as cv
+import os
+import matplotlib.pylab as plt
+import face_recognition
+from PIL import Image,ImageDraw
+
+plt.style.use('ggplot')
+train_dir = '/Users/jatinarora/deepfake-detection-challenge/train_sample_videos/'
+fig, ax = plt.subplots(1,1, figsize=(15, 15))
+train_video_files = [train_dir + x for x in os.listdir(train_dir)]
+# video_file = train_video_files[30]
+
+## Getting the first frame
+video_file = '/Users/jatinarora/deepfake-detection-challenge/train_sample_videos/akxoopqjqz.mp4'
+cap = cv.VideoCapture(video_file)
+success, image = cap.read()
+image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+cap.release()
+ax.imshow(image)
+ax.xaxis.set_visible(False)
+ax.yaxis.set_visible(False)
+ax.title.set_text(f"FRAME 0: {video_file.split('/')[-1]}")
+plt.grid(False)
+plt.show()
+face_locations = face_recognition.face_locations(image)
+print("I found {} face(s) in this photograph.".format(len(face_locations)))
+
+for face_location in face_locations:
+
+    # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+
+    # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    fig, ax = plt.subplots(1,1, figsize=(5, 5))
+    plt.grid(False)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    ax.imshow(face_image)
+    plt.show()
+
+face_landmarks_list = face_recognition.face_landmarks(image)
+pil_image = Image.fromarray(image)
+d = ImageDraw.Draw(pil_image)
+
+for face_landmarks in face_landmarks_list:
+
+    # Print the location of each facial feature in this image
+    for facial_feature in face_landmarks.keys():
+        print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+
+    # Let's trace out each facial feature in the image with a line!
+    for facial_feature in face_landmarks.keys():
+        d.line(face_landmarks[facial_feature], width=3)
+
+# Show the picture
+pil_image.show()
